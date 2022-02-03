67e3ce6ae4e533a5994c45935e2cbc8b9b66cafe
Yann <yann.forner@hotmail.com>
2022-02-03 16:43:46 +0100

    Adjust

--- App/src/main/java/com/example/restservice/RestServiceApplication.java
+++ App/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
+        System.out.println("Running on http://localhost:8080");
     }
+
 
 }
