3cda62b2c0ae23b9d77ae80a74f073b53aade760
QuentinBeauchet <quentinbeauchet@free.fr>
2022-02-09 22:16:31 +0100

    Spring fonctionel

---
+++ src/main/java/fr/uca/springbootstrap/controllers/ModuleController.java
@@ -0,0 +1,80 @@
+package fr.uca.springbootstrap.controllers;
+
+import fr.uca.springbootstrap.models.ERole;
+import fr.uca.springbootstrap.models.Module;
+import fr.uca.springbootstrap.models.Role;
+import fr.uca.springbootstrap.models.User;
+import fr.uca.springbootstrap.payload.request.SignupRequest;
+import fr.uca.springbootstrap.payload.response.MessageResponse;
+import fr.uca.springbootstrap.repository.ModuleRepository;
+import fr.uca.springbootstrap.repository.RoleRepository;
+import fr.uca.springbootstrap.repository.UserRepository;
+import fr.uca.springbootstrap.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/module")
+public class ModuleController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	ModuleRepository moduleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/{id}/participants/{userid}")
+	@PreAuthorize("hasRole('TEACHER')")
+	public ResponseEntity<?> addUser(Principal principal, @PathVariable long id, @PathVariable long userid) {
+		Optional<Module> omodule = moduleRepository.findById(id);
+		Optional<User> ouser = userRepository.findById(userid);
+		if (!omodule.isPresent()) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: No such module!"));
+		}
+		if (!ouser.isPresent()) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: No such user!"));
+		}
+
+		Module module = omodule.get();
+		User user = ouser.get();
+		User actor = userRepository.findByUsername(principal.getName()).get();
+
+		Set<User> participants = module.getParticipants();
+		if ((participants.isEmpty() && actor.equals(user))
+				|| participants.contains(actor)) {
+			participants.add(user);
+		} else {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Not allowed to add user!"));
+		}
+		moduleRepository.save(module);
+		return ResponseEntity.ok(new MessageResponse("User successfully added to module!"));
+	}
+}
