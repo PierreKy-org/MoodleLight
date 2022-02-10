a9ba4d639748bbcc209acbf86a1aaa61668f3fc1
Yann <yann.forner@hotmail.com>
2022-02-10 10:48:33 +0100

    adding resources to the gitignore

--- src/main/java/fr/uca/springbootstrap/SpringBootSecurityPostgresqlApplication.java
+++ src/main/java/fr/uca/springbootstrap/SpringBootSecurityPostgresqlApplication.java
@@ -8,6 +8,7 @@ public class SpringBootSecurityPostgresqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityPostgresqlApplication.class, args);
+		System.out.println("running on http://localhost:8080");
 	}
 
 
