66de3833df8f71538b575b488f9a2f7f056f0c3c
QuentinBeauchet <quentinbeauchet@free.fr>
2022-02-11 14:28:10 +0100

    html

--- src/main/java/fr/uca/springbootstrap/repository/UserRepository.java
+++ src/main/java/fr/uca/springbootstrap/repository/UserRepository.java
@@ -3,15 +3,9 @@ package fr.uca.springbootstrap.repository;
 import java.util.List;
 import java.util.Optional;
 
-import fr.uca.springbootstrap.models.Role;
 import fr.uca.springbootstrap.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -20,7 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Boolean existsByUsername(String username);
 
   Boolean existsByEmail(String email);
-  @Transactional
-  List<User> findAll();
 
+  List<User> findAll();
 }
