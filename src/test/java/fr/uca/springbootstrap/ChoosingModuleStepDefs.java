8253a4363a3de7a36fffec49a70b785e79266b14
michael-marynowicz <michaelmarynowicz5@gmail.com>
2022-02-11 20:46:56 +0100

    cucumber ChoosingModule done #1

--- src/test/java/fr/uca/springbootstrap/ChoosingModuleStepDefs.java
+++ src/test/java/fr/uca/springbootstrap/ChoosingModuleStepDefs.java
@@ -1,6 +1,8 @@
 package fr.uca.springbootstrap;
 import fr.uca.springbootstrap.controllers.AuthController;
+import fr.uca.springbootstrap.models.ERole;
 import fr.uca.springbootstrap.models.Module;
+import fr.uca.springbootstrap.models.Role;
 import fr.uca.springbootstrap.models.User;
 import fr.uca.springbootstrap.repository.ModuleRepository;
 import fr.uca.springbootstrap.repository.RoleRepository;
@@ -11,6 +13,9 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -32,6 +37,14 @@ public class ChoosingModuleStepDefs {
     PasswordEncoder encoder;
 
 
+    @When("{string} chooses the module {string}")
+    public void iChooseTheModule(String userName, String moduleName) {
+        User user = userRepository.findByUsername(userName).orElseThrow(() -> new RuntimeException("Error: User is not found."));
+        Module module = moduleRepository.findByName(moduleName).orElseThrow(() -> new RuntimeException("Error: Module is not found."));
+        user.addModule(module);
+        userRepository.save(user);
+        moduleRepository.save(module);
+    }
 
     @And("{string} can register to {string}")
     public void iCanRegisterTo(String userName, String moduleName) {
@@ -53,6 +66,8 @@ public class ChoosingModuleStepDefs {
         User student = userRepository.findByUsername(studentName).orElseThrow(() -> new RuntimeException("Error: User is not found."));
         Module module = moduleRepository.findByName(moduleName).orElseThrow(() -> new RuntimeException("Error: Module is not found."));
         teacher.addUserToModule(student, module);
+        moduleRepository.save(module);
+        userRepository.save(student);
         assertTrue(module.getParticipants().contains(student));
         assertTrue(student.getModules().contains(module));
     }
@@ -72,7 +87,27 @@ public class ChoosingModuleStepDefs {
 
     @Given("A Module named {string} with no Teacher registered")
     public void aModuleNamedWithNoTeacherRegistered(String moduleName) {
-        Module module = moduleRepository.findByName(moduleName).orElseThrow(() -> new RuntimeException("Error: Module is not found."));
-        assertEquals(1,module.getParticipants().size());
+        Module module = moduleRepository.findByName(moduleName).orElse(new Module(moduleName));
+        for(User user : module.getParticipants()){
+            for(Role role : user.getRoles()){
+                if (role.getName().equals(ERole.ROLE_TEACHER)){
+                    module.getParticipants().remove(user);
+                }
+            }
+
+        }
+        assertEquals(0,module.getNbrOfTeacher());
+        moduleRepository.save(module);
     }
+
+    @When("the student {string} choose the module {string}")
+    public void theStudentChooseTheModule(String student, String moduleName) {
+        User user = userRepository.findByUsername(student).orElseThrow(() -> new RuntimeException("Error: User is not found."));
+        Module module = moduleRepository.findByName(moduleName).orElseThrow(() -> new RuntimeException("Error: Module is not found."));
+        user.addModule(module);
+        moduleRepository.save(module);
+        userRepository.save(user);
+    }
+
+
 }
