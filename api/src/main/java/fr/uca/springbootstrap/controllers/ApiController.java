55481b417ae4f637b3e77c2f441f269a5a51c4cd
QuentinBeauchet <quentinbeauchet@free.fr>
2022-02-17 23:12:37 +0100

    nettoyage

--- api/src/main/java/fr/uca/springbootstrap/controllers/ApiController.java
+++ api/src/main/java/fr/uca/springbootstrap/controllers/ApiController.java
@@ -1,22 +1,13 @@
 package fr.uca.springbootstrap.controllers;
 
-import fr.uca.springbootstrap.payload.request.JythonRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
-
-import static fr.uca.springbootstrap.SpringBootSecurityPostgresqlApplication.RUNNER;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -33,19 +24,4 @@ public class ApiController {
             return "Aucune documentation disponible";
         }
     }
-
-    @GetMapping("/api/test")
-    public String test() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet request = new HttpGet("http://"+RUNNER+":8080/jython/test");
-        try {
-            HttpResponse latestHttpResponse = httpClient.execute(request);
-            String response = EntityUtils.toString(latestHttpResponse.getEntity());
-            System.out.println(response);
-            return response;
-        } catch (IOException e) {
-            return "ERROR";
-        }
-    }
-
 }
